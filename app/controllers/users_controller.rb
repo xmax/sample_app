d85679ce311707492ddba7deef893d63a1584ab5
Max Bielikov <ua.xmax@gmail.com>
2011-10-16 14:58:16 +0300

    Go home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,21 @@ class UsersController < ApplicationController
     def show
 	@user = User.find( params[:id] )
 	@title = @user.name
-    end
+    end #show
 
     def new
+	@user = User.new
 	@title = "Sign up"
-    end
+    end #new
+    
+    def create
+	@user = User.new( params[:user] )
+	if @user.save
+	    #
+	else
+	    @title = "Sign up"
+	    render 'new'
+	end
+    end #create
 
 end
