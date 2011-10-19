e0bcb03a34827b6d587679328da800d6c6c5dc8d
Max Bielikov <ua.xmax@gmail.com>
2011-10-19 17:37:54 +0300

    Go HOME

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -84,6 +84,14 @@ describe UsersController do
 			get :show, :id => @user
 			response.should have_selector( "h1>img", :class => "gravatar" )
 		end
+		
+		it "should show the user's micropost" do
+			mp1 = Factory( :micropost, :user => @user, :content => "Foo bar" )
+			mp2 = Factory( :micropost, :user => @user, :content => "Baz foo" )
+			get :show, :id => @user
+			response.should have_selector( "span.content", :content => mp1.content )
+			response.should have_selector( "span.content", :content => mp2.content )
+		end
 	end #describe "GET 'show'"
 #===================================================    
 	describe "GET 'new'" do
